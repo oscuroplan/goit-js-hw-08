6b9dca3f154486dd6267616fdff621fd5c076041
Maxim Levandovskiy <Levan.Maxim@gmail.com>
2021-11-18 13:01:37 +0200

    1-e задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+//инициализируем (достучаться до узла)
+const iframe = document.querySelector('#vimeo-player');
+
+//создаем экземпляр
+const iframePlayer = new Vimeo.Player(iframe);
+
+// Обновляем в локальном хранилище текущую позицию
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+    // data is an object containing properties specific to that event
+};
+
+// Получаем текущую из локального хранилища
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+// устанавливаем на плеере сохраненную текущую позицию
+iframePlayer.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = фактическое время, которое искал player 
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // время было меньше 0 или больше, чем продолжительность 
+            break;
+
+        default:
+            //произошла другая ошибка
+            break;
+    }
+});
+
+// отслеживаем событие timeupdate
+//При запуске просмотра вызываем обработку и передаем текущие 
+ // секунды в onPlay с интервалом в 1 секунду
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
+//data - это объект, содержащий свойства, специфичные для этого события 
